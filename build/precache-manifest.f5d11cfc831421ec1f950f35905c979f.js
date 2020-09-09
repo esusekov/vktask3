d6e66cb18a89c1b63726e3d598e0ec9e30711ca8
Denis Antonov <antonov25041992@yandex.ru>
2020-09-09 15:10:48 +0300

    deploy github pages

---
+++ build/precache-manifest.f5d11cfc831421ec1f950f35905c979f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40132e595a39a7bc0bbaaf49e20943d",
+    "url": "/VK-vezdecode-task-3/index.html"
+  },
+  {
+    "revision": "e969f8528908d393cbae",
+    "url": "/VK-vezdecode-task-3/static/css/main.794fb4fc.chunk.css"
+  },
+  {
+    "revision": "3653dceec2bb1395e4ce",
+    "url": "/VK-vezdecode-task-3/static/js/2.84911def.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/VK-vezdecode-task-3/static/js/2.84911def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e969f8528908d393cbae",
+    "url": "/VK-vezdecode-task-3/static/js/main.2994f14e.chunk.js"
+  },
+  {
+    "revision": "1a89b99a9f50eafe27a2",
+    "url": "/VK-vezdecode-task-3/static/js/runtime-main.c73b915a.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/VK-vezdecode-task-3/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/VK-vezdecode-task-3/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/VK-vezdecode-task-3/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/VK-vezdecode-task-3/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/VK-vezdecode-task-3/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "06277842ccc3f7c2a37085189dacdcbc",
+    "url": "/VK-vezdecode-task-3/static/media/check-symbol.06277842.svg"
+  },
+  {
+    "revision": "751f84c25a8d1ff159f499cbb9aa5bb7",
+    "url": "/VK-vezdecode-task-3/static/media/edit.751f84c2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/VK-vezdecode-task-3/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/VK-vezdecode-task-3/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "1198ed41690cc0974aa8baa0e95ebd1b",
+    "url": "/VK-vezdecode-task-3/static/media/refresh-button.1198ed41.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/VK-vezdecode-task-3/static/media/sub.4eb05fdd.png"
+  }
+]);
